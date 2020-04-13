0c25176721b8abdaf91f3a00e5c9c343fa62c2bc
Dima Lis <readspore@gmail.com>
2020-04-13 13:12:27 +0300

    create joystick

---
+++ Assets/Scripts/v1/joystick/TouchPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public enum ControllType {EditTransfotm, AddForce };
+public class TouchPlayerController : MonoBehaviour
+{
+    public ControllType controllType = ControllType.EditTransfotm;
+    // Update is called once per frame
+    void Update()
+    {
+            Debug.Log("y  "+ MobileJoystick_UI.instance.moveDirection.y + " X  " + MobileJoystick_UI.instance.moveDirection.x);
+        //Move Front/Back
+        if (MobileJoystick_UI.instance.moveDirection.y != 0)
+        {
+            //transform.Translate(transform.forward * Time.deltaTime * 2.45f * MobileJoystick_UI.instance.moveDirection.y, Space.World);
+        }
+
+        //Rotate Left/Right
+        if (MobileJoystick_UI.instance.moveDirection.x != 0)
+        {
+            //transform.Rotate(new Vector3(0, 14, 0) * Time.deltaTime * 4.5f * MobileJoystick_UI.instance.moveDirection.x, Space.Self);
+        }
+    }
+}
