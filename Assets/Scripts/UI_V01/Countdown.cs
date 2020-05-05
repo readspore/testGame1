9c6e854e1b506d286c78ce67d800cb489f5edf05
Michael <michaelshamrai@gmail.com>
2020-05-05 14:35:32 +0300

    UI: Countdown

---
+++ Assets/Scripts/UI_V01/Countdown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    float CurrentTime = 0f;
+    float StartingTime = 15f;
+
+    [SerializeField] Text CountdownText;
+
+    private void Start()
+    {
+        CurrentTime = StartingTime;
+    }
+
+    void Update()
+    {
+        CurrentTime -= 1 * Time.deltaTime;
+
+        if(CurrentTime <= 60 && CurrentTime >= 10)
+        {
+            CountdownText.text = ("00:" + CurrentTime.ToString("0"));
+        }
+        else if(CurrentTime <= 9)
+        {
+            CountdownText.text = ("00:0" + CurrentTime.ToString("0"));
+        }
+
+        if (CurrentTime < 0)
+        {
+            CurrentTime = 0;
+        }
+    }
+  
+}
