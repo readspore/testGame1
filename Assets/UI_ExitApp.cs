93d4235f4490862b36336c136cf46f5796b0170b
Michael <michaelshamrai@gmail.com>
2020-05-05 10:38:37 +0300

    UI: Close scripts

---
+++ Assets/UI_ExitApp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ExitApp : MonoBehaviour
+{
+    public GameObject ExitPanel;
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            ExitPanel.SetActive(true);
+        }
+    }
+}
---
+++ Assets/UI_ReturnControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ReturnControl : MonoBehaviour
+{
+    public GameObject CurrentMenu;
+    public GameObject PreviousMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PreviousMenu.SetActive(true);
+            CurrentMenu.SetActive(false);
+        }
+    }
+}
