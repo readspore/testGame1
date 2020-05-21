b640c65e7fb1f362aa74223362525740f5e184ee
Michael <michaelshamrai@gmail.com>
2020-05-21 21:02:08 +0300

    UI: Forge panel fix

---
+++ Assets/Scripts/UI_V01/PiecesText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PiecesText : MonoBehaviour
+{
+    public Text InventoryQuantity;
+    public Text Pieces;
+    int ConvertedQuantity;
+    string Piece1 = "piece";
+    string Piece2 = "pieces";
+
+
+    void FixedUpdate()
+    {
+        ConvertedQuantity = Convert.ToInt32(InventoryQuantity.text);
+        if (ConvertedQuantity <= 1)
+        {
+            Pieces.text = Piece1;
+        }
+        else
+        {
+            Pieces.text = Piece2;
+        }
+    }
+}
